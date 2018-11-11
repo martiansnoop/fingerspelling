5c17bf555b52c67d8251d234b89d6cd8ef3b3b37
Emily Wenberg <martiansnoop@gmail.com>
2018-11-11 13:08:58 -0800

    Add a service worker, mostly for fun
    I think it's ok to cache the number of images I'm caching.
    Eventually I want to cache the other stuff too, but I update the main
    script often since I'm still writing them, so I don't want to deal with
    deleting them from the cache every time I change them.

---
+++ images/list.sh
@@ -0,0 +1,5 @@
+#! /bin/bash -e
+for x in {a..z}
+do
+    echo "\"images/$x.gif\","
+done
---
+++ sw.js
@@ -0,0 +1,51 @@
+const cacheName = "cache-v1";
+const urlsToCache = [
+	"images/a.gif",
+	"images/b.gif",
+	"images/c.gif",
+	"images/d.gif",
+	"images/e.gif",
+	"images/f.gif",
+	"images/g.gif",
+	"images/h.gif",
+	"images/i.gif",
+	"images/j.gif",
+	"images/k.gif",
+	"images/l.gif",
+	"images/m.gif",
+	"images/n.gif",
+	"images/o.gif",
+	"images/p.gif",
+	"images/q.gif",
+	"images/r.gif",
+	"images/s.gif",
+	"images/t.gif",
+	"images/u.gif",
+	"images/v.gif",
+	"images/w.gif",
+	"images/x.gif",
+	"images/y.gif",
+	"images/z.gif"
+];
+
+self.addEventListener("install", function(event) {
+	event.waitUntil(
+		caches.open(cacheName)
+			.then(cache => {
+				return cache.addAll(urlsToCache);	
+			})
+	);
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+                if (response) {
+                    return response;
+                }				
+                return fetch(event.request);
+			})
+	);
+});
+
